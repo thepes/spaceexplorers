24fdbbb3e8eae2de1264aba6f281ea4812be1dbc
Sébastien Delaire <the.pes@gmail.com>
2016-09-05 15:50:31 +0200

    - work on network games

--- app/src/main/java/org/copains/spaceexplorer/NewGameSelectorActivity.java
+++ app/src/main/java/org/copains/spaceexplorer/NewGameSelectorActivity.java
@@ -1,7 +1,11 @@
 package org.copains.spaceexplorer;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,6 +17,9 @@ import java.util.List;
 
 public class NewGameSelectorActivity extends Activity {
 
+    private List<Game> pendingGames;
+    private int selectedGame;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +27,7 @@ public class NewGameSelectorActivity extends Activity {
 
         ListView gamesView = (ListView)findViewById(R.id.pending_games_listview);
         // TODO: implement multi player and player count selection
-        List<Game> pendingGames = GameMg.getPendingGames(1);
+        pendingGames = GameMg.getPendingGames(1);
         List<String> games = new ArrayList<>();
         for (Game g : pendingGames) {
             games.add("" + g.getId() + " (" + g.getFreeSlots() + "/" + g.getMaxPlayers() + ")");
@@ -28,5 +35,20 @@ public class NewGameSelectorActivity extends Activity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(NewGameSelectorActivity.this,
                 android.R.layout.simple_list_item_1, games);
         gamesView.setAdapter(adapter);
+        gamesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("spaceexplorers","selected game : " +i);
+                Log.i("spaceexplorers","Game : " + pendingGames.get(i).getId());
+               //adapterView.setBackgroundColor(R.color.spaceexplorers_blue);
+                for (int idx = 0 ; idx < pendingGames.size(); idx++) {
+                    adapterView.getChildAt(idx).setBackgroundColor(getResources().getColor(R.color.spaceexplorers_blue));
+                }
+                view.setSelected(true);
+                view.setBackgroundColor(Color.LTGRAY);
+                selectedGame = i;
+            }
+        });
     }
+
 }
